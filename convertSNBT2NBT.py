25b5dbfc4feffa15cd73def846b8c8d2315a8816
Simo Berrada Rekhami <simorberrada@gmail.com>
2025-12-18 00:33:04 +0100

    implementing structures

--- convertSNBT2NBT.py
+++ convertSNBT2NBT.py
@@ -2,7 +2,7 @@ import nbtlib
 from nbtlib import parse_nbt
 
 # 1. Charger ton fichier SNBT brut
-with open("pillage_outpost.snbt", "r", encoding="utf-8") as f:
+with open("ggg.snbt", "r", encoding="utf-8") as f:
     snbt_text = f.read()
 
 # 2. Parser le SNBT en structure NBT (renvoie un Compound avec size/palette/blocks/entities/DataVersion)
@@ -12,6 +12,6 @@ compound = parse_nbt(snbt_text)
 nbt_file = nbtlib.File(compound)
 
 # 4. Sauvegarder en NBT GZIPPÉ (important pour les structures dans data/<modid>/structures)
-nbt_file.save("orc_camp_small_1.nbt", gzipped=True)
+nbt_file.save("ggg.nbt", gzipped=True)
 
-print("Conversion réussie ! Fichier généré : orc_camp_small_1.nbt")
+print("Conversion réussie ! Fichier généré : ggg.nbt")
